c7330e83bd555edc6addd1d7e322b217cca0ef1c
Itsnidasaifi <nidasaifi001@gmail.com>
2024-06-23 20:03:21 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,83 @@
+// const express = require("express");
+// const { MongoClient } = require("mongodb");
+// const mongoose = require("mongoose")
+// const cors = require("cors");
+// const postApi = require("./routes/post");
+// const userApi = require("./routes/user");
+// const profileApi = require("./routes/profile");
+// const bodyparser  = require("body-parser")
+
+// const app = express();
+// const PORT = process.env.PORT || 8080;
+// app.use(cors({
+//   origin: ['http://localhost:3000']
+// }));
+// app.use(bodyparser.urlencoded({ extended: true }))
+// app.use(bodyparser.json())
+// app.use(express.json());
+// app.use("/api/posts", postApi);
+// app.use("/api/users", userApi);
+// app.use("/api/profile",profileApi);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello from Express!");
+// });
+
+// mongoose.connect(
+//   // "mongodb+srv://root:root@cluster0.0mlseyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+//   // {
+//   //   bufferCommands: false,
+//   //   useNewUrlParser: true,
+//   //   useUnifiedTopology: true
+
+//   // }
+//   `mongodb://127.0.0.1:27017/Gmail-Data`,({ useNewUrlParser:true })
+// )
+//   .then((client) => {
+//     console.log("Connected to MongoDB Compass");
+
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on http://localhost:${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to MongoDB Atlas", err);
+//   });
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const postApi = require("./routes/post");
+const userApi = require("./routes/user");
+const profileApi = require("./routes/profile");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors({ origin: ['http://localhost:3000'] }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.use("/api/posts", postApi);
+app.use("/api/users", userApi);
+app.use("/api/profile", profileApi);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/Gmail-Data', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log("Connected to MongoDB locally");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
